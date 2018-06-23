cac1412808c6e69a0ca2955446d8fb0f8cc7bfb4
arun-ghontale <arun.g.ghontale@gmail.com>
2018-06-23 16:56:12 +0530

    adding controllers

---
+++ TodosController/todoscontroller.js
@@ -0,0 +1,67 @@
+let todos = [
+  {
+    id: Math.floor(Math.random() * 100),
+    title: "create todos app",
+    completed: false
+  },
+  {
+    id: Math.floor(Math.random() * 100),
+    title: "fetch milk",
+    completed: false
+  },
+  {
+    id: Math.floor(Math.random() * 100),
+    title: "Water the garden",
+    completed: false
+  }
+];
+
+const handlers = {
+  getTodos: function(req, res) {
+    res.render("index", {
+      todos: todos || []
+    });
+  },
+  postTodos: function(req, res) {
+    // console.log(req.body);
+    let todo = {
+      id: Math.floor(Math.random() * 100),
+      title: req.body.todo,
+      completed: false
+    };
+
+    todos.push(todo);
+    // console.log(todos);
+    res.redirect("/todos");
+  },
+  deleteTodos: function(req, res) {
+    // console.log("\n\n\n\n");
+    // console.log(req.params.id);
+    // console.log(req.body);
+    todos = todos.filter(todo => {
+      return todo.id !== Number(req.params.id.trim());
+    });
+    res.redirect("/todos");
+  },
+  putTodos: function(req, res) {
+    // console.log(req.params.id);
+    // console.log(req.body);
+    todos = todos.map(todo => {
+      if (todo.id === Number(req.params.id.trim())) {
+        return {
+          id: todo.id,
+          title: todo.title,
+
+          /*checkbox key wouldn't exist if the checkbox value is off*/
+
+          completed: "checkbox" in req.body ? true : false
+        };
+      } else {
+        return todo;
+      }
+    });
+    res.redirect("/todos");
+  }
+};
+
+module.exports = handlers;
--- index.js
+++ index.js
@@ -1,70 +1,47 @@
 let express = require("express");
 let path = require("path");
+let methodOverride = require("method-override");
+let todoscontroller = require("./TodosController/todoscontroller.js");
 
 let app = express();
-app.use(express.json());
-app.use(express.urlencoded({extended : true}));
-app.set("views",path.join(__dirname,"views"));
-app.set("view engine","pug");
 
-let todos = [{
-  id: Math.floor(Math.random()*100),
-  title: 'create todos app',
-  completed: false
-},
-{
-  id: Math.floor(Math.random()*100),
-  title: 'fetch milk',
-  completed: false
-},
-{
-  id: Math.floor(Math.random()*100),
-  title: 'Water the garden',
-  completed: false
-}
-];
+/*middleware to parse JSON*/
+app.use(express.json());
+/*Working with forms and query strings*/
+app.use(express.urlencoded({ extended: true }));
+/*Support DELETE and PUT methods*/
+app.use(
+  methodOverride(function(req, res) {
+    console.log(req.body);
+    if (req.body && typeof req.body == "object" && "_method" in req.body) {
+      let method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
+/*Static directory path set*/
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 /*update*/
-app.put("/todos/:id",function(req,res){
-  console.log(req.params.id)
-  res.render("index",{
-    todos : req.query.message || 'default hey'
-  })
-})
+app.put("/todos/:id", todoscontroller.putTodos);
 
 /*delete*/
-app.delete("/todos/:id",function(req,res){
-  console.log(req.params.id)
-  console.log(req.body)
-  let todo = req.params.todoName
-  res.render("index",{
-    title : 'delete successful'
-  })
-})
+app.delete("/todos/:id", todoscontroller.deleteTodos);
 
 /*create*/
-app.post("/todos",function(req,res){
-  console.log(req.body)
-  let todo = {
-    id: Math.floor(Math.random()*100),
-    title:req.body.todo,
-    completed:false
-  };
-
-  todos.push(todo);
-  console.log(todos)
-  res.render("index",{
-    todos : todos || []
-  })
-})
-
+app.post("/todos", todoscontroller.postTodos);
 
 /*read*/
-app.get("/todos",function(req,res){
-  res.render("index",{
-    todos : todos || []
-  })
-})
+app.get("/todos", todoscontroller.getTodos);
 
-console.log("Running on port 3000")
+// app.get("/todos", function(req, res) {
+//   res.render("index", {
+//     todos: todos || []
+//   });
+// });
+
+console.log("Running on port 3000");
 app.listen(3000);
